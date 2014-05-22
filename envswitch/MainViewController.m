4adba8c1d3b1f2fa7a4a8121e50cc07911013317
Carlos Yaconi H <carlos@forkhq.com>
2014-05-22 11:13:20 -0400

    Fixes a bug replacing keys.

--- envswitch/MainViewController.m
+++ envswitch/MainViewController.m
@@ -193,7 +193,7 @@ int const localhost=3;
             [self replaceInText:nodeConfig usingRegexExp:@"host = .*" withText: [NSString stringWithFormat: @"host = %@", url]];
             [self replaceInText:nodeConfig usingRegexExp:@"protocol = .*" withText: [NSString stringWithFormat: @"protocol = %@", protocol]];
             if (apiKey != nil)
-                [self replaceInText:nodeConfig usingRegexExp:@"api_key = .*" withText: [NSString stringWithFormat: @"api_key = '%@'", apiKey]];
+                [self replaceInText:nodeConfig usingRegexExp:@"api_key = .*" withText: [NSString stringWithFormat: @"api_key = %@", apiKey]];
             if (deviceKey != nil){
                 if (![deviceKey isEqualToString:@""]) deviceKey = [NSString stringWithFormat: @"'%@'", deviceKey];
                 [self replaceInText:nodeConfig usingRegexExp:@"device_key = .*" withText: [NSString stringWithFormat: @"device_key = %@", deviceKey]];
